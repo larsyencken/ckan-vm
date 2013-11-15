81bf47287f88ba4fc2f186ef0f3b18c0b89f0859
Lars Yencken <lars@yencken.org>
2013-11-15 15:48:36 +1100

    Add more steps towards configuring CKAN.

--- marelle-deps/ckan.pl
+++ marelle-deps/ckan.pl
@@ -4,6 +4,15 @@
 %
 %  Dependencies for setting up CKAN on Ubuntu 12.04.
 %
+%  Follows instructions from:
+%  http://docs.ckan.org/en/latest/install-from-package.html
+%
+
+meta_pkg('ckan-server-standalone', [
+    'postgresql',
+    'openjdk-6-jdk',
+    'solr configured'
+]).
 
 pkg(ckan).
 met(ckan, linux(_)) :- check_dpkg('python-ckan').
@@ -22,3 +31,16 @@ managed_pkg(nginx).
 managed_pkg(apache2).
 managed_pkg('libapache2-mod-wsgi').
 managed_pkg(libpq5).
+managed_pkg(postgresql).
+managed_pkg('solr-jetty').
+managed_pkg('openjdk-6-jdk').
+
+pkg('solr configured').
+met('solr configured', linux(_)) :-
+    bash('diff -q /vagrant/config/jetty /etc/default/jetty').
+meet('solr configured', linux(_)) :-
+    bash('sudo cp -f /vagrant/config/jetty /etc/default/jetty'),
+    bash('sudo rm -f /etc/solr/conf/schema.xml'),
+    bash('sudo ln -s /usr/lib/ckan/default/src/ckan/ckan/config/solr/schema-2.0.xml /etc/solr/conf/schema.xml'),
+    bash('sudo service jetty restart').
+depends('solr configured', linux(_), ['solr-jetty']).
--- setup_ckan.sh
+++ setup_ckan.sh
@@ -6,3 +6,6 @@ if [ ! -x /usr/bin/curl ]; then
 fi
 
 curl -s "https://raw.github.com/larsyencken/marelle/master/bootstrap_global.sh" | bash
+
+cd /vagrant
+marelle meet ckan-server-standalone
